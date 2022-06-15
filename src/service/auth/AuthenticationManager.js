588cd4284cfdb35bbbc6a4f2d35984086e5f5aa2
Anton Nikitenko <anikitenko@uvic.ca>
2022-06-15 13:41:12 -0700

    Modified vega-web to route requests properly for production

--- src/service/auth/AuthenticationManager.js
+++ src/service/auth/AuthenticationManager.js
@@ -2,5 +2,5 @@ import {doPost} from '../BaseAPI.js';
 
 export function login(userInfo){
 	console.log("In Auth", userInfo);
-	return doPost(process.env.REACT_APP_VEGA_WEB_SERVER_URL + "/api/login", userInfo);
+	return doPost(process.env.REACT_APP_API_URL + "/api/login", userInfo);
 } 
