3f6e3b69f89fe2b7e975340f5e5aca69eaca0c57
BrodyGimson <brody.gimson@gmail.com>
2022-06-22 10:40:39 -0700

    Added initial vega vault file

---
+++ src/components/pages/VegaVault.js
@@ -0,0 +1,8 @@
+import SimplePageLayout from '../templates/SimplePageLayout.js';
+const VegaVault = (props) => {
+	return (
+		<SimplePageLayout>
+		</SimplePageLayout>
+		);
+}
+export default VegaVault;
