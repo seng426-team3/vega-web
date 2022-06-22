272bb0fb8facd468fc4d1f99b87d9d23c87d0df3
BrodyGimson <brody.gimson@gmail.com>
2022-06-22 12:45:09 -0700

    Add table headers to vault page

--- src/components/pages/VegaVault.js
+++ src/components/pages/VegaVault.js
@@ -1,7 +1,20 @@
 import SimplePageLayout from '../templates/SimplePageLayout.js';
+import {Table} from 'react-bootstrap';
+
 const VegaVault = (props) => {
 	return (
 		<SimplePageLayout>
+			<Table>
+				<thead>
+					<tr>
+						<td>Name</td>
+						<td>Creation Date</td>
+						<td>Data</td>
+					</tr>
+				</thead>
+				<tbody>
+				</tbody>
+			</Table>
 		</SimplePageLayout>
 		);
 }
