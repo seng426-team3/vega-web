486bbe85fef2c7e0d2852cd47a79da3b96f9039a
jesselacroix394 <jesselacroix394@gmail.com>
2022-06-24 17:07:25 -0700

    Added secrets container
    Added secrets container to the vega vault page with a scroll bar

--- src/components/pages/VegaVault.js
+++ src/components/pages/VegaVault.js
@@ -3,6 +3,7 @@ import {Button, Table} from 'react-bootstrap';
 import {UserContext} from '../../auth/UserProvider.js';
 import {useContext} from 'react';
 import {withRouter} from 'react-router-dom'
+import './VegaVault.css'
 
 const VegaVault = (props) => {
 	const {user} = useContext(UserContext);
@@ -11,9 +12,37 @@ const VegaVault = (props) => {
 		props.history.push("/login");
 	}
 
+	const listOfSecrets = [["name", "2022-06-24", "txt"], ["name2", "1999-09-19", "txt"],
+		["name", "2022-06-24", "txt"], ["name2", "1999-09-19", "txt"],
+		["name", "2022-06-24", "txt"], ["name2", "1999-09-19", "txt"],
+		["name", "2022-06-24", "txt"], ["name2", "1999-09-19", "txt"],
+		["name", "2022-06-24", "txt"], ["name2", "1999-09-19", "txt"],
+		["name", "2022-06-24", "txt"], ["name2", "1999-09-19", "txt"]];
+
+	const listOfSecretsHTML = () => {
+		if(listOfSecrets.length){
+			return listOfSecrets.map(secretData=>
+				<tr>
+					<td>{secretData[0]}</td>
+					<td>{secretData[1]}</td>
+					<td>{secretData[2]}</td>
+				</tr>)
+		}
+	}
+
+	const scrollBox ={
+		display: 'block'
+	};
+
+	const scrollBar = {
+		position: 'relative',
+		height: '200px',
+		overflow: 'auto'
+	};
+
 	var page;
 	
-	if (user.role != "ROLE_STAFF" || user.role != "ROLE_USER" || user.role != "ROLE_ADMIN") {
+	if (!(user.role != "ROLE_STAFF" || user.role != "ROLE_USER" || user.role != "ROLE_ADMIN")) {
 		page = 
 			<SimplePageLayout>
 				<h3>Vega Vault service is only available to registered Users</h3>
@@ -25,17 +54,20 @@ const VegaVault = (props) => {
 	} else {
 		page = 
 			<SimplePageLayout>
-				<Table>
-					<thead>
-						<tr>
-							<td>Name</td>
-							<td>Creation Date</td>
-							<td>Data</td>
-						</tr>
-					</thead>
-					<tbody>
-					</tbody>
-				</Table>
+				<div className="table-wrapper-scroll-y my-custom-scrollbar">
+					<Table className = "table table-striped mb-0">
+						<thead>
+							<tr>
+								<td>Name</td>
+								<td>Creation Date</td>
+								<td>Data</td>
+							</tr>
+						</thead>
+						<tbody>
+							{listOfSecretsHTML()}
+						</tbody>
+					</Table>
+				</div>
 			</SimplePageLayout>;
 	}
 
