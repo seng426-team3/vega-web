57df0b889e1e7811c0765a128ea13fdad3dc18ef
Samantha Kosman <skosman@uvic.ca>
2022-06-26 00:11:44 -0700

    Delete unnecessary page

--- src/components/pages/AboutUs.js
+++
@@ -1,6 +0,0 @@
-import SimplePageLayout from "../templates/SimplePageLayout.js";
-import ListRoundedImages from "../UI/molecules/ListRoundImages.js";
-const AboutUs = (props) => {
-  return <SimplePageLayout></SimplePageLayout>;
-};
-export default AboutUs;
