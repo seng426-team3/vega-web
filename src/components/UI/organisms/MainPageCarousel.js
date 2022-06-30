360450d32257bac8f525357d5ba71e4c5ddd6bcb
kianapaz <kianapaz021@gmail.com>
2022-06-30 16:41:40 -0700

    Fixed horizontal footer alignment

--- src/components/UI/organisms/MainPageCarousel.js
+++ src/components/UI/organisms/MainPageCarousel.js
@@ -22,8 +22,8 @@ const MainPageCarousel = (props) => {
           height="600"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3>Vega Vault is ultra Secure</h3>
+          <p>We're all about security.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -35,8 +35,8 @@ const MainPageCarousel = (props) => {
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h3>News Articles Updated Daily</h3>
+          <p>Enjoy the best security articles updated daily.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -48,9 +48,9 @@ const MainPageCarousel = (props) => {
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <h3>Vega Vault Secrets</h3>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            Add secrets and share them with friends.
           </p>
         </Carousel.Caption>
       </Carousel.Item>
