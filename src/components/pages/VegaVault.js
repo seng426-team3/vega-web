7e62515d7d0c48546ee4b18f8b07a947c52a054b
jesselacroix394 <jesselacroix394@gmail.com>
2022-06-30 17:52:45 -0700

    Added sort to table

--- src/components/pages/VegaVault.js
+++ src/components/pages/VegaVault.js
@@ -1,7 +1,7 @@
 import SimplePageLayout from '../templates/SimplePageLayout.js';
 import {Button} from 'react-bootstrap';
 import {UserContext} from '../../auth/UserProvider.js';
-import {useContext, useMemo, useState} from 'react';
+import {useCallback, useContext, useMemo, useRef, useState} from 'react';
 import {withRouter} from 'react-router-dom'
 import { AgGridReact } from 'ag-grid-react';
 import './VegaVault.css';
@@ -25,31 +25,46 @@ const VegaVault = (props) => {
 		props.history.push("/edit-secret-form");
 	};
 
-	const [listOfSecrets] = useState([
+	var listOfSecrets = [
 		{Name: "Secret stuff", CreationDate: "2022-06-27", Data: "txt"}, {Name: "Dog", CreationDate: "2020-05-25", Data: "png"},
 		{Name: "Passwords", CreationDate: "2013-03-16", Data: "txt"}, {Name: "Holiday Hawaii", CreationDate: "2019-02-20", Data: "png"},
 		{Name: "CSC426Report1", CreationDate: "2022-06-05", Data: "txt"}, {Name: "Cat", CreationDate: "2020-05-26", Data: "png"},
 		{Name: "CSC426Report2", CreationDate: "2022-06-19", Data: "txt"}, {Name: "Holiday Spain", CreationDate: "2010-04-14", Data: "png"},
 		{Name: "CSC426Report3", CreationDate: "2022-07-02", Data: "txt"}, {Name: "Meme", CreationDate: "2022-06-27", Data: "png"},
 		{Name: "Final Exam", CreationDate: "2021-12-15", Data: "txt"}, {Name: "Camping 2021", CreationDate: "2021-07-01", Data: "png"}
-	]);
+	];
 
-	const [columnDefs] = useState([
-		{field: 'Name', width: 100, editable: true},
-		{field: 'CreationDate', width: 100},
-		{field: 'Data', width: 100},
-		//{field: 'Manage', width: 100, sortable: false}
-	]);
+	const gridRef = useRef(null);
+	const [rowData, setRowData] = useState(listOfSecrets);
+
+	const containerStyle = useMemo(() => ({width: "100%", height: "100%"}), []);
+	const gridStyle = useMemo(() => ({width: "100%", height: "100%"}), []);
 
 	const defaultColDef = useMemo( () => ({
 		sortable: true,
 		flex: 1,
 		resizeable: true,
 		minWidth: 100
-	}))
+	}));
 
-	const containerStyle = useMemo(() => ({width: "100%", height: "100%"}), []);
-	const gridStyle = useMemo(() => ({width: "100%", height: "100%"}), []);
+	const [columnDefs] = useState([
+		{field: 'Name', width: 100, editable: true},
+		{field: 'CreationDate', width: 100},
+		{field: 'Data', width: 100}
+	]);
+
+	const removeSelected = useCallback(() => {
+
+		const selectedRowNodes = gridRef.current.api.getSelectedNodes();
+		const selectedIDs = selectedRowNodes.map(function (rowNode){
+			return rowNode.id;
+		});
+		const filterData = rowData.filter(function (dataItem){
+			return selectedIDs.indexOf(dataItem.Name) < 0;
+		});
+		setRowData(filterData);
+
+	}, [rowData]);
 
 	var page;
 	
@@ -71,12 +86,17 @@ const VegaVault = (props) => {
 					<button onClick={goToCreateSecret} className="button-blue" size = "sm">+ New Secret</button>
 					<button onClick={goToEditSecret} className="button-blue" size = "sm">Edit Secret</button>
 					<button className="button-blue" size = "sm">Share Secret</button>
-					<button className="button-red" size = "sm">Delete Secret</button>
+					<button onClick={removeSelected} className="button-red" size = "sm">Delete Secret</button>
 				</div>
 				<div style={containerStyle}>
 					<div style={{height: '475px', boxSizing: 'border-box'}}>
 						<div style = {gridStyle} className="ag-theme-alpine">
-							<AgGridReact rowData={listOfSecrets} columnDefs={columnDefs} defaultColDef={defaultColDef} rowSelection={'multiple'}>
+							<AgGridReact ref={gridRef}
+										 rowData={rowData}
+										 columnDefs={columnDefs}
+										 defaultColDef={defaultColDef}
+										 rowSelection={'multiple'}
+										 animateRows={true}>
 							</AgGridReact>
 						</div>
 					</div>
