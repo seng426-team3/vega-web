b8d97b9721623d08e4622f723ed985360586d0dd
BrodyGimson <brody.gimson@gmail.com>
2022-07-04 12:49:08 -0700

    Add tests with TO-DO comments

--- src/tests/seleniumVegaVaultPage.test.js
+++ src/tests/seleniumVegaVaultPage.test.js
@@ -62,6 +62,127 @@ describe("Users must be able to fetch their secrets, use CRUD operations on them
         await expect(secret_name).toEqual("Super Secret");
     });
 
+    /*
+    Scenario: User successfully creates a new secret
+        Given I am a logged in user
+        And I am on the “Manage Secrets” page
+        And I have selected a file I wish to upload as a secret
+        When I click upload
+        Then a confirmation message should appear that the file was uploaded as a secret
+        And the new secret should be added to my list of secrets
+    */
+    it("User should be able to create a secret and have it appear in their secret list", async() => {
+        // Given
+        await signIn("testuser2@venus.com");
+
+        // When
+        await driver.get(reactAppURL + "vega-vault");
+
+        // Then
+        // TO-DO write creation test
+    });
+
+    /*
+    Scenario: User reads a secret
+        Given I am a logged in user
+        And I am on the “Manage Secrets” page
+        And I have selected a secret I wish to read
+        When I click “Read”
+        Then the secret’s data is shown to me
+    */
+        it("User should be able to read a secret from their secret list", async() => {
+            // Given
+            await signIn("testuser2@venus.com");
+    
+            // When
+            await driver.get(reactAppURL + "vega-vault");
+    
+            // Then
+            // TO-DO write reading test
+        });
+
+    /*
+    Scenario: User updates a secret
+        Given I am a logged in user 
+        And I am on the “Manage Secrets” page
+        And I have selected a secret I wish to update
+        When I fill in info I wish to update
+        And click “Update”
+        Then the secret I selected is updated with the info I filled in
+        And a confirmation message will display confirming it was successfully updated
+    */
+    it("User should be able to update a secret from their secret list", async() => {
+        // Given
+        await signIn("testuser2@venus.com");
+
+        // When
+        await driver.get(reactAppURL + "vega-vault");
+
+        // Then
+        // TO-DO write updating test
+    });
+
+    /* 
+    Scenario: User deletes a secret
+	    Given I am a logged in user 
+        And I am on the “Manage Secrets” page
+        And I have selected a secret I wish to delete
+        When I click “Delete”
+        And confirm I wish to delete it
+        Then the secret I selected is deleted
+        And a confirmation message will display that it was deleted
+    */
+    it("User should be able to delete a secret from their secret list", async() => {
+        // Given
+        await signIn("testuser2@venus.com");
+
+        // When
+        await driver.get(reactAppURL + "vega-vault");
+
+        // Then
+        // TO-DO write deletion test
+    });
+
+    /* 
+    Scenario: User shares secret with another user
+        Given I am a logged in user
+        And I am on the “Manage Secrets” page
+        And I have selected a secret I wish to share
+        When I select a user to share it with
+        And click “Share”
+        Then the secret is sent to the user I selected
+        And a confirmation message is displayed to show it was sent
+    */
+    it("User should be able to share a secret from their secret list with another user", async() => {
+        // Given
+        await signIn("testuser2@venus.com");
+
+        // When
+        await driver.get(reactAppURL + "vega-vault");
+
+        // Then
+        // TO-DO write sharing test
+    });
+
+    /*
+    Scenario: User filters secrets by date
+        Given I am a logged in user 
+        And I am on the “Manage Secrets” page
+        When I click “Filter By Date”
+        And enter a date from/date to entry
+        Then my list of secrets will update to only display secrets between those dates
+    */
+        it("User should be able to share a secret from their secret list with another user", async() => {
+            // Given
+            await signIn("testuser2@venus.com");
+    
+            // When
+            await driver.get(reactAppURL + "vega-vault");
+    
+            // Then
+            // TO-DO write filtering test
+        });
+
     afterEach(async () => {
         await driver.quit();
     }, 15000);
