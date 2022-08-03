bcbd2f7cac42a024f59aa54c9ad730590d1f9e0c
jesse <jesselacroix394@gmail.com>
2022-08-03 15:16:43 -0700

    Fixed login issue with HTTPS

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.disable('x-powered-by');
 
 if (process.env.NODE_ENV === 'development') {
   var corsOptions = {
-    origin: 'http://127.0.0.1:3000',
+    origin: 'https://127.0.0.1:3000',
     optionsSuccessStatus: 200
   };
   app.use(cors(corsOptions));
